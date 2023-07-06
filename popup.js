125fa30a95769a1fed649551a4452b6fcd4fabde
Nick Hsu <nick@NickdeMacBook-Pro.local>
2023-07-06 18:39:23 +0800

    Support adding venue with TW address

--- popup.js
+++ popup.js
@@ -75,26 +75,42 @@
       document.addEventListener("DOMContentLoaded", function() {
         const tab1 = document.getElementById("tab1");
         const tab2 = document.getElementById("tab2");
+        const tab3 = document.getElementById("tab3");
         const tab1Content = document.getElementById("tab1-content");
         const tab2Content = document.getElementById("tab2-content");
+        const tab3Content = document.getElementById("tab3-content");
 
         tab1.classList.add("active");
         tab1Content.style.display = "block";
         tab2Content.style.display = "none";
+        tab3Content.style.display = "none";
       
         tab1.addEventListener("click", function() {
           tab1.classList.add("active");
           tab2.classList.remove("active");
+          tab3.classList.remove("active");
           tab1Content.style.display = "block";
           tab2Content.style.display = "none";
+          tab3Content.style.display = "none";
         });
       
         tab2.addEventListener("click", function() {
           tab1.classList.remove("active");
           tab2.classList.add("active");
+          tab3.classList.remove("active");
           tab1Content.style.display = "none";
           tab2Content.style.display = "block";
+          tab3Content.style.display = "none";
         });
+
+        tab3.addEventListener("click", function() {
+            tab1.classList.remove("active");
+            tab2.classList.remove("active");
+            tab3.classList.add("active");
+            tab1Content.style.display = "none";
+            tab2Content.style.display = "none";
+            tab3Content.style.display = "block";
+          });
       });
 
       getApPassBtn?.addEventListener('click', async () => {
@@ -373,6 +389,8 @@
                         const jwtToken = `Bearer ${jwtString}` 
                         myHeaders.append("Authorization", jwtToken);
                         myHeaders.append("Content-Type", 'application/json');
+                        myHeaders.append("Accept", 'application/json, text/plain, */*');
+                        //myHeaders.append("Origin", location.origin);
                         myHeaders.append("Access-Control-Allow-Origin", '*');
                         //const jsessionId = document.cookie.match(/JSESSIONID=([^;]+)/); // Get the JSESSIONID cookie value from the current tab
 
@@ -408,136 +426,77 @@
                         .catch(error => {
                           console.log(error);
                         });
+                        
+                        const addVenueName = document.getElementById("addVenueName").value;
+                        const messageElement = document.getElementById("addVenueResult");
+                        const requestElement = document.getElementById("addVenueRequestId");
 
-                        function getVenueNameById(id) {
-                          // Iterate over the groups array
-                          for (var i = 0; i < venueGroups.length; i++) {
-                          var group = venueGroups[i];
-                          // Check if the group's id matches the given id
-                          if (group.id === id) {
-                          // Return the user associated with the matching id
-                          return group.name;
-                          }
-                          }
-                          // If no matching id is found, return null or handle the case as needed
-                          return null;
+                        function displayMessageId(message, id, color) {
+                            messageElement.textContent = message;
+                            messageElement.style.color = color;
+                            requestElement.textContent = 'Request ID: ' + id;
+                            requestElement.style.color = color;
                         }
 
-                        function AppendApPasswordById(id, password) {
-                          // Iterate over the groups array
-                          for (var i = 0; i < venueGroups.length; i++) {
-                            var group = venueGroups[i];
-                            // Check if the group's id matches the given id
-                            if (group.id === id) {
-                            // Return the user associated with the matching id
-                              group.password = password
+                        function displayMessage(message, color) {
+                            messageElement.textContent = message;
+                            messageElement.style.color = color;
+                        }
+
+                        const addVenuePayload = {
+                            "name":addVenueName,
+                            "address":{
+                                "addressLine":"No. 45, City Hall Rd, Xinyi District, Taipei City, Taiwan 110",
+                                "city":"Xinyi District, Taipei City",
+                                "country":"Taiwan",
+                                "latitude":25.0341222,"longitude":121.5604212,
+                                "timezone":"Asia/Taipei"
                             }
-                          }
-                          // If no matching id is found, return null or handle the case as needed
-                          return null;
-                        }
+                        };
 
-                        const request2 = new Request(apiUrlWifi, {
-                          method: "GET",
+                        const request2 = new Request(apiUrlVenue, {
+                          method: "POST",
                           headers: myHeaders,
-                          credentials: 'include'
+                          credentials: 'include',
+                          body: JSON.stringify(addVenuePayload)
                         });
                         console.log(request2);
 
                         //fetch(request).then(resp => console.log(resp))
                         fetch(request2)
-                        .then(response => response.json())
-                        .then(data => {
-                          console.log(data); // Process the response data as needed
-                          // Parse id and apPassword from the response
-                          const wifiArray=data.venues
-                          for (var i = 0; i < wifiArray.length; i++) {
-                              var element2 = wifiArray[i];
-                              var api2VenueId=element2.id
-                              var api2ApPassword=element2.apPassword
-                              AppendApPasswordById(api2VenueId, api2ApPassword);
-                              //console.log("id: " + api2VenueId + ", AP Pass: " + api2ApPassword);
-                              //var matchVenueName = getVenueNameById(api2VenueId);
-                              //console.log('Name: ' + matchVenueName + ' ,AP Password: ' + api2ApPassword + ' ,id: ' + api2VenueId)
-                          }
-                          // Get the container element
-                          var groupContainer = document.getElementById('groupContainer');
-                          groupContainer.innerHTML = '';
-
-                          venueGroups.sort(function(a, b) {
-                              var nameA = a.name.toUpperCase();
-                              var nameB = b.name.toUpperCase();
-                              if (nameA < nameB) {
-                                return -1;
+                        .then(response => {
+                        //.then(response => response.json())
+                        //.then(data => {
+                          //console.log(data); // Process the response data as needed
+                          // Parse id and apPassword from the response   
+                          if (response.status === 202) {
+                            console.log('202');
+                            response.json().then(responseData => {
+                              if (responseData.requestId.length > 0) {
+                                const successMessage = "Success"
+                                const getRequestId =  responseData.requestId;
+                                displayMessageId(successMessage, getRequestId, "green");
+                              } else {
+                                displayMessage("Success", "green");
                               }
-                              if (nameA > nameB) {
-                                return 1;
+                            });
+                          } else if (response.status === 400 || response.status === 403) {
+                            console.log('40X');
+                            response.json().then(responseData => {
+                              if (responseData.errors && responseData.errors.length > 0) {
+                                const errorMessage = responseData.errors[0].message;
+                                const getRequestId =  responseData.requestId;
+                                displayMessageId(errorMessage, getRequestId, "red");
+                              } else {
+                                displayMessage("Unknown Error", "red");
                               }
-                              return 0;
-                          });
-
-                          var table = document.querySelector('#groupTable');
-                          var headerRow = document.querySelector('#headerRow');
-
-                          // Clear any existing content in the header row
-                          headerRow.innerHTML = '';
-                          // Get the keys of the first group object
-                          var groupKeys = Object.keys(venueGroups[0]);
-
-                          // Iterate over the keys and create table header cells
-                          for (var i = 0; i < groupKeys.length; i++) {
-                              var key = groupKeys[i];
-
-                              // Create a new table header cell
-                              var headerCell = document.createElement('th');
-
-                              // Set the content of the header cell as the key
-                              headerCell.textContent = key;
-
-                              // Append the header cell to the header row
-                              headerRow.appendChild(headerCell);
-                          }
-
-                          // Get the table body element
-                          var tableBody = table.querySelector('tbody');
-
-                          // Clear any existing rows in the table body
-                          tableBody.innerHTML = '';
-
-                          // Iterate over the groups array
-                          for (var i = 0; i < venueGroups.length; i++) {
-                              var group = venueGroups[i];
-                              /*
-                              // Create a new paragraph element to display user and pass
-                              var paragraph = document.createElement('p');
-
-                              // Set the content of the paragraph using user and pass values
-                              paragraph.textContent = 'Venue: ' + group.name + ' , AP Password: ' + group.password + ' , id: ' + group.id;
-
-                               // Append the paragraph to the container element
-                              groupContainer.appendChild(paragraph);
-                              */
-                              //for table data
-                              // Create a new row element
-                              var row = document.createElement('tr');
-
-                              // Iterate over the keys and create table cells for each key
-                              for (var j = 0; j < groupKeys.length; j++) {
-                                  var key = groupKeys[j];
-  
-                                  // Create a new table cell
-                                  var cell = document.createElement('td');
-  
-                                  // Set the content of the cell using the corresponding value from the group object
-                                  cell.textContent = group[key];
-  
-                                  // Append the cell to the row
-                                  row.appendChild(cell);
-                              }
-
-                               // Append the row to the table body
-                              tableBody.appendChild(row);
-                          }
+                            }).catch(error => {
+                              console.error("Error parsing response JSON:", error);
+                              displayMessage("Unknown Error", "red");
+                            });
+                          } else {
+                            displayMessage("Unknown Error", "red");
+                          }             
                         })
                         .catch(error => {
                           console.log(error);
